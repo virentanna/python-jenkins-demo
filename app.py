3e87d798d9f0cfb2fad6479d2a6924d6e582928e
Viren Tanna <viren121@gmail.com>
2026-06-17 00:31:51 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ def add_numbers(a, b):
     return a + b
 
 if __name__ == "__main__":
-    result = add_numbers(51, 5)
+    result = add_numbers(51, 15)
     print(f"The calculation result is: {result}")
