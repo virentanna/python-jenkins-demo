7d73da4066bca3e36520ee5f4f04d98f2e29ba48
Viren Tanna <viren121@gmail.com>
2026-06-18 00:42:20 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 def add_numbers(a, b):
     return a + b
 
+'''
+    Sample Project to understand Jenkins (CI/CD) Concepts
+'''
 if __name__ == "__main__":
     result = add_numbers(51, 15)
     print(f"The calculation result is: {result}")
